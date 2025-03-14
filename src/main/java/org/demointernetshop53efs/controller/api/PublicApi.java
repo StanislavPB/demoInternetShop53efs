d8349965538796063bbe76d11c9e0464313e191d
StanislavPB <perminovsb@gmail.com>
2025-03-14 16:45:57 +0000

    add swagger annotation

--- src/main/java/org/demointernetshop53efs/controller/api/PublicApi.java
+++ src/main/java/org/demointernetshop53efs/controller/api/PublicApi.java
@@ -1,6 +1,12 @@
 package org.demointernetshop53efs.controller.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
+import org.demointernetshop53efs.dto.ErrorResponseDto;
 import org.demointernetshop53efs.dto.UserRequestDto;
 import org.demointernetshop53efs.dto.UserResponseDto;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +15,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/public")
 public interface PublicApi {
 
+
+    @Operation(summary = "Регистрация пользователя", description = "Операция доступна всем, роль по умолчанию - USER")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Пользователь успешно зарегистрирован",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "Ошибка валидации",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class)))
+    })
     @PostMapping("/register")
     public ResponseEntity<UserResponseDto> userRegistration(@Valid @RequestBody UserRequestDto request);
 
--- src/main/java/org/demointernetshop53efs/controller/api/UserApi.java
+++ src/main/java/org/demointernetshop53efs/controller/api/UserApi.java
@@ -1,5 +1,11 @@
 package org.demointernetshop53efs.controller.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.demointernetshop53efs.dto.ErrorResponseDto;
 import org.demointernetshop53efs.dto.UserResponseDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +15,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/api/users")
 public interface UserApi {
 
+    @Operation(summary = "Получение информации о пользователе по ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Информация о пользователе",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserResponseDto.class))),
+            @ApiResponse(responseCode = "404", description = "Пользователь не найден",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class)))
+    })
     @GetMapping("/{userId}")
     public ResponseEntity<UserResponseDto> getUserById(@PathVariable long userId);
 
